9618257def56ec8e3d183f755dd6fa1a4f5134f5
Aleksandr-Rusin <ru5nedug@mail.ru>
2021-06-07 17:30:05 +0300

    animation

--- Home/FD1-155-21/Makets_flex/js/main.js
+++ Home/FD1-155-21/Makets_flex/js/main.js
@@ -1,26 +1,100 @@
 $(function () {
-    $(window).scroll(function() {
-        $('.anim__start__1').each(function(){
-            var imagePos = $(this).offset().top;
+	$(window).scroll(function() {
+	    $('.fadeIn-Down').each(function(){
+	        var imagePos = $(this).offset().top;
 //  знаем высоту от начала страницы до необходимого блока
-            var topOfWindow = $(window).scrollTop();
+	        var topOfWindow = $(window).scrollTop();
 //  узнаем высоту от начала страницы
-            if (imagePos < topOfWindow+650) {
-                $(this).addClass("anim__features__1");
-            }
-        });
-        $('.anim__start__2').each(function(){
-            var imagePos = $(this).offset().top;
-            var topOfWindow = $(window).scrollTop();
-            if (imagePos < topOfWindow+650) {
-                $(this).addClass("anim__features__2");
-            }
-        });
-    });
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInDown");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.fadeIn-Up').each(function(){
+	        var imagePos = $(this).offset().top;
 
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.note-1').each(function(){
+	        var imagePos = $(this).offset().top;
 
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInDownBig");
+	        }
+	    });
+	});$(window).scroll(function() {
+	    $('.note-2').each(function(){
+	        var imagePos = $(this).offset().top;
 
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRightBig");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.note-3').each(function(){
+	        var imagePos = $(this).offset().top;
 
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUpBig");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.note-4').each(function(){
+	        var imagePos = $(this).offset().top;
 
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeftBig");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.fadeIn-Left').each(function(){
+	        var imagePos = $(this).offset().top;
 
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.fadeIn-Right').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInRight");
+	        }
+	    });
+	});
+		$(window).scroll(function() {
+	    $('.fade-In').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeIn");
+	        }
+	    });
+	});
 })
+jQuery(document).ready(function() {
+		jQuery("a.scrollto").click(function () {
+				elementClick = jQuery(this).attr("href")
+				destination = jQuery(elementClick).offset().top;
+				jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
+				return false;
+		});
+});
