f1579fb21a3577c174be11c9a70f4e788748fcc6
Aleksandr-Rusin <ru5nedug@mail.ru>
2021-06-21 13:53:24 +0300

    overlay

--- Home/FD1-155-21/Makets_flex/js/menu_grid.js
+++ Home/FD1-155-21/Makets_flex/js/menu_grid.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
         $('.menu-burger__header').toggleClass('open-menu');
         $('.menu__ul').toggleClass('open-menu');
         $('.wrapper').toggleClass('open-menu');
+        $('.overlay').toggleClass('open-menu');
         // $('body').toggleClass('fixed-page');
 	});
 });
