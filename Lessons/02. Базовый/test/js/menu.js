9ad7e8bdf28355fa21700bc84dcec5ab335536ee
Aleksandr-Rusin <ru5nedug@mail.ru>
2021-06-24 13:42:55 +0300

    Появление ФОРМЫ РЕГИСТРАЦИИ по клику на LOGIN

--- Lessons/02. Базовый/test/js/menu.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$('.menu-burger__header').click(function(){
-        $('.menu-burger__header').toggleClass('open-menu');
-        $('.menu__ul').toggleClass('open-menu');
-        $('.wrapper').toggleClass('open-menu');
-        // $('body').toggleClass('fixed-page');
-	});
-});
