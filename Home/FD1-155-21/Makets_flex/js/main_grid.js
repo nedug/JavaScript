8eba665d76f7d85cb48f07385e06a7ef75602e3d
Aleksandr-Rusin <ru5nedug@mail.ru>
2021-07-02 15:49:33 +0300

    язык

--- Home/FD1-155-21/Makets_flex/js/main_grid.js
+++ Home/FD1-155-21/Makets_flex/js/main_grid.js
@@ -94,6 +94,62 @@ $(document).ready(function()
 });
 
 
+//  ПЕРЕКЛЮЧЕНИE ЯЗЫКА
+$(document).ready(function()
+{
+    $('#language').change(function()
+    {
+        if(this.checked!=true)
+        {
+            document.getElementById("man").innerHTML = "mens";
+            document.getElementById("man").style.fontSize = "23px";
+            document.getElementById("man").style.letterSpacing = "7px";
+            document.getElementById("women").innerHTML = "womens";
+            document.getElementById("women").style.fontSize = "23px";
+            document.getElementById("women").style.letterSpacing = "7px";
+            document.getElementById("sale").innerHTML = "sale";
+            document.getElementById("sale").style.fontSize = "23px";
+            document.getElementById("sale").style.letterSpacing = "7px";
+            document.getElementById("login").innerHTML = "Login";
+            document.getElementById("login").style.fontSize = "23px";
+            document.getElementById("login").style.letterSpacing = "7px";
+            document.getElementById("marquee").innerHTML = "Black Friday Sale. up to 50% off + 1 month!";
+            document.getElementById("shop").innerHTML = "SHOP NOW";
+            document.getElementById("Features").innerHTML = "Features";
+            document.getElementById("Delivery").innerHTML = "Delivery";
+            document.getElementById("Shipping").innerHTML = "Shipping";
+            document.getElementById("Returns").innerHTML = "Returns";
+            document.getElementById("feat_1").innerHTML = "When you move, 600 muscles in your body work to make you more flexible and stronger. Sports are very important for children and teenagers. When their bones are well held by the muscles, they grow and become stronger. <br><br>\n" +
+                "\n" +
+                "\t\t\t\t\t\t\t<span>Sports maintain the whole body: your brain, your lungs, and your heart. For doing that your body uses your fats and your sugar. You get more healthy and slimmer. That’s not all. You develop your abilities such as catching a ball or keeping balance when you ride a bike. You learn new skills which are useful in your life. You get more successful and progress-oriented.</span>";
+            document.getElementById("deliv_2").innerHTML = "In summer I have to go by bicycle when there is a traffic jam. It takes about an hour and a half. It’s a bit long but much faster than walking. I have not to complain too much because the weather forecast is good for this week. And if there is a lot of traffic jams, I will get to the office and enjoy the nice weather.";
+        }
+        else {
+            document.getElementById("man").innerHTML = "мужчинам";
+            document.getElementById("man").style.fontSize = "19px";
+            document.getElementById("man").style.letterSpacing = "4px";
+            document.getElementById("women").innerHTML = "женщинам";
+            document.getElementById("women").style.fontSize = "19px";
+            document.getElementById("women").style.letterSpacing = "4px";
+            document.getElementById("sale").innerHTML = "скидки";
+            document.getElementById("sale").style.fontSize = "19px";
+            document.getElementById("sale").style.letterSpacing = "4px";
+            document.getElementById("login").innerHTML = "войти";
+            document.getElementById("login").style.fontSize = "19px";
+            document.getElementById("login").style.letterSpacing = "4px";
+            document.getElementById("marquee").innerHTML = "Черная Пятница. 50% скидки + 1 месяц!";
+            document.getElementById("shop").innerHTML = "купить сейчас";
+            document.getElementById("Features").innerHTML = "принципы";
+            document.getElementById("Delivery").innerHTML = "доставка";
+            document.getElementById("Shipping").innerHTML = "Перевозка";
+            document.getElementById("Returns").innerHTML = "Возврат";
+            document.getElementById("feat_1").innerHTML = "Спорт поддерживает все тело: ваш мозг, легкие и сердце. Для этого ваше тело использует ваши жиры и сахар. Вы станете здоровее и стройнее. Это не все. Вы развиваете свои способности, такие как ловля мяча или удержание равновесия во время езды на велосипеде. Вы осваиваете новые навыки, которые пригодятся вам в жизни. Вы становитесь более успешными и ориентированными на прогресс.";
+            document.getElementById("deliv_2").innerHTML = "Летом мне приходится ехать на велосипеде в пробку. На это уходит около полутора часов. Это немного длинновато, но намного быстрее, чем ходить. Я не должен сильно жаловаться, потому что прогноз погоды на эту неделю хороший. А если будет много пробок, доберусь до офиса и буду наслаждаться хорошей погодой.";
+        }
+    });
+});
+
+
 //  Включение ПЕРЕМЕЩЕНИЯ Модального ОКНА
 $(document).ready(function()
 {
