ed677609590ac56270727f092c9cbc1a986ada73
Aleksandr-Rusin <ru5nedug@mail.ru>
2021-10-27 14:42:10 +0300

    Update index.js

--- Home/FD2-98-21/project/spa_test/js/index.js
+++ Home/FD2-98-21/project/spa_test/js/index.js
@@ -23,7 +23,7 @@ const routes = {
 const mySPA = (function() {
 
 
-    /* ------- begin model ------- */
+    /* -------  model ------- */
     function ModuleModel() {
 
         let myModuleView = null;
@@ -49,7 +49,7 @@ const mySPA = (function() {
         }
 
 
-        this.saveData = function(inputName, inputDate, inputNumCig, inputCostCig) { // Получаем данные из модалки и сохраняем в объект 'userData'
+        this.saveData = function(inputName, inputDate, inputNumCig, inputCostCig) { // Получаем данные пользователя и сохраняем в объект 'userData'
 
             // if (inputDay > 31 || inputDay < 1 || inputMonth > 12 || inputMonth < 1 || inputYear < 1 || inputYear > new Date().getFullYear()) { /* Проверка на корректность данных */
             //
@@ -65,153 +65,100 @@ const mySPA = (function() {
             }
 
             this.storeData(); /* Сохраняем данные в localStorage */
-
-            // myModalView.validDate(true);
         }
 
 
-        this.storeData = function() { // Сохраняем данные в localStorage
+        this.storeData = function() { /* Сохраняем данные в localStorage */
 
             let userDataSerial = JSON.stringify(userData);
 
             localStorage.setItem('userData', userDataSerial);
-
-            // myModuleView.renderContentLogin();
-
-            // console.log(pageNameId)
-
-            // userDataStorage = JSON.parse(localStorage.getItem('userData'));
-            //
-            // myModuleView.renderContent(pageNameLink, userDataStorage);
 
             this.updateState(pageNameLink); /* Обновляем приложение */
 
         }
 
 
-        // this.getData = function(pageName) { // Достаем данные из хранилища
-        //
-        //     pageNameLink = pageName;
-        //
-        //     userDataStorage = JSON.parse(localStorage.getItem('userData'));
-        //
-        //     // this.updateState();
-        //
-        //     // if (!userDataStorage) return; /* Проверяем на наличие данных в localStorage */
-        //
-        //     // this.updateState(pageNameLink);
-        //     myModuleView.renderContent(pageNameLink, userDataStorage);
-        // }
-
-
-        this.clearData = function() { // Очищаем данные в хранилище
-
+        this.clearData = function() { /* Очищаем данные в хранилище */
 
             localStorage.removeItem('userData');
 
-
-            this.updateState(pageNameLink);
+            this.updateState(pageNameLink); /* Обновляем приложение */
         }
+
 
     }
 
 
 
-    /* ------- begin view -------- */
+    /* ------- view -------- */
     function ModuleView() {
+
         let myModuleContainer = null;
         let menu = null;
         let contentContainer = null;
         let routesObj = null;
         let that = this;
-
         let userStorage = null;
 
+
         this.init = function(container, routes) {
+
             myModuleContainer = container;
             routesObj = routes;
             menu = myModuleContainer.querySelector("#mainmenu");
             contentContainer = myModuleContainer.querySelector("#content");
         }
 
+
         this.renderContent = function(hashPageName, userData) {
 
             let routeName = "default";
-
             userStorage = userData;
 
-            // console.log(userStorage);
-
             if (hashPageName.length > 0 && hashPageName !== 'login' && !userStorage) {
+                // console.log(1111)
                 routeName = "authorization";
-                console.log(1111)
-                something();
+                updateContent();
             }
-            // else if ( )
+
             else if ((hashPageName === '' || hashPageName === 'login') && !userStorage) {
-
-                console.log(2222)
+                // console.log(2222)
                 routeName = hashPageName in routes ? hashPageName : "login";
-                // contentContainer.innerHTML = routesObj[routeName].render(`${routeName}-page`);
-                something();
-                // this.renderContentLogin();
+                updateContent();
             }
-            else if ((hashPageName === '') && userStorage) {
 
-                console.log(333)
+            else if (hashPageName === '' && userStorage) {
+
+                // console.log(333)
                 routeName = "main";
-                // contentContainer.innerHTML = routesObj[routeName].render(`${routeName}-page`);
-                something();
-                // this.renderContentLogin();
+                updateContent();
             }
-            else if ((hashPageName === 'login') && userStorage) {
 
-                console.log(444)
+            else if (hashPageName === 'login' && userStorage) {
+                // console.log(444)
                 routeName = "options";
-                // contentContainer.innerHTML = routesObj[routeName].render(`${routeName}-page`);
-                something();
-                // this.renderContentLogin();
+                updateContent();
             }
+
             else {
-                console.log(555)
+                // console.log(555)
                 routeName = hashPageName in routes ? hashPageName : "error";
-                something();
+                updateContent();
             }
 
-            // console.log(hashPageName);
-            // console.log(routeName);
+            function updateContent() {
 
-
-
-
-
-            function something() {
                 window.document.title = routesObj[routeName].title;
-
-                // console.log(routesObj)
-                // console.log(routesObj[routeName].render(`${routeName}-page`))
-                // console.log(routesObj[routeName].id)
-
                 contentContainer.innerHTML = routesObj[routeName].render(`${routeName}-page`);
-
-                // contentContainer.innerHTML = routesObj['options11'].render();
-
                 that.updateButtons(routesObj[routeName].id);
             }
-
-
-
-            // console.log(JSON.parse(localStorage.getItem('userData')))
-
-            // if(JSON.parse(localStorage.getItem('userData'))) {
-            //     this.renderContentLogin();
-            // }
         }
 
-        this.updateButtons = function(currentPage) {
-            const menuLinks = menu.querySelectorAll(".mainmenu__link");
 
-            // console.log(currentPage)
+        this.updateButtons = function(currentPage) {
+
+            const menuLinks = menu.querySelectorAll(".mainmenu__link");
 
             for (let link of menuLinks) {
                 currentPage === link.getAttribute("href").slice(1) ? link.classList.add("active") : link.classList.remove("active");
@@ -219,31 +166,11 @@ const mySPA = (function() {
         }
 
 
-        this.renderContentLogin = function() {
-
-            const linkLogin = myModuleContainer.querySelector('[href="#login"]');
-            linkLogin.innerHTML = 'Настройки';
-            // linkLogin.setAttribute('href', '#fff');
-
-            const h3 = myModuleContainer.querySelector(".user__content");
-            h3.innerHTML = 'Изменить данные пользователя:';
-
-            const inputNumCig = myModuleContainer.querySelector(".input__num-cigarette");
-            inputNumCig.value = JSON.parse(localStorage.getItem('userData')).userNumCigarette;
-
-
-            const buttonSave = myModuleContainer.querySelector(".data__save");
-            buttonSave.innerHTML = 'Изменить';
-
-
-
-
-        }
     }
 
 
 
-    /* ----- begin controller ---- */
+    /* ----- controller ---- */
     function ModuleController() {
 
         let myModuleContainer = null;
@@ -260,46 +187,24 @@ const mySPA = (function() {
             myModuleContainer = container;
             myModuleModel = model;
 
-            // вешаем слушателей на событие hashchange и кликам по пунктам меню
-            window.addEventListener("hashchange", this.updateState);
+            window.addEventListener("hashchange", this.updateState); // вешаем слушателей на событие hashchange и кликам по пунктам меню
 
             this.updateState(); //первая отрисовка
-
-
-
-
-            // const buttonSave = myModuleContainer.querySelector(".data__save");
-            // buttonSave.addEventListener("click", this.saveModal);
-            //
-            // inputName = myModuleContainer.querySelector(".input__default");
-            // inputDate = myModuleContainer.querySelector(".input__date-last");
-            // inputNumCig = myModuleContainer.querySelector(".input__num-cigarette");
-            // inputCostCig = myModuleContainer.querySelector(".input__cost-cigarette");
 
 
             myModuleContainer.addEventListener("click", clickHandler);
 
             function clickHandler(e) {
 
-                let target = e.target;
-
-                // console.log(target.getAttribute('class'))
-
-                if (target.getAttribute('class') === 'data__save') {
-
-                    // console.log('текст')
+                if (e.target.getAttribute('class') === 'data__save') {
 
                     that.saveDataUser();
                 }
 
-                if (target.getAttribute('class') === 'data__delete') {
-
-                    // console.log('текст')
+                if (e.target.getAttribute('class') === 'data__delete') {
 
                     that.clearData();
                 }
-
-
             }
         }
 
@@ -312,7 +217,7 @@ const mySPA = (function() {
         }
 
 
-        this.saveDataUser = function () { /* Сохраняем данные из инпутов в localStorage */
+        this.saveDataUser = function() { /* Сохраняем данные из инпутов в localStorage */
 
             inputName = myModuleContainer.querySelector(".input__default");
             inputDate = myModuleContainer.querySelector(".input__date-last");
@@ -323,10 +228,12 @@ const mySPA = (function() {
         }
 
 
-        this.clearData = function () { /* Очищаем данные из localStorage */
+        this.clearData = function() { /* Очищаем данные из localStorage */
 
             myModuleModel.clearData();
         }
+
+
     }
 
 
